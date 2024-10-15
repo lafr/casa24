4b9a2109584cc62a009d423af098cbfaae3b0992
lafr <lafrodrigues@gmail.com>
2024-10-15 19:04:23 -0300

    Delete menus directory

--- menus/menu_admin.php
+++
@@ -1,8 +0,0 @@
-<p>
-    <div style="display: flex; justify-content: space-around; padding: 10px; background-color: #f1f1f1;">
-        <button onclick="location.href='page1.php'">Button 1</button>
-        <button onclick="location.href='page2.php'">Button 2</button>
-        <button onclick="location.href='page3.php'">Button 3</button>
-        <button onclick="location.href='page4.php'">Button 4</button>
-    </div>
-</p>
