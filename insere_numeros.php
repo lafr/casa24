4da039d450729ce1a3d9da63d7fe6eb13460aa29
Luiz Augusto <lafrodrigues@gmail.com>
2024-11-06 09:36:23 -0300

    20241106-favicon

--- insere_numeros.php
+++ insere_numeros.php
@@ -2,6 +2,8 @@
 
     /**
      * Formulário para inserção de números em paradas e rotina para inserir na base
+     * 
+     * Sempre atualiza os números, mesmo quando se entra uma segunda vez
      */
 
     include 'header.php';
@@ -26,30 +28,30 @@
 ?>
 
 <div class='bloco_centro'>
-    <fieldset>
     <div class='content'>
+    <fieldset>
         <h2>Inserir números em <?php echo $_GET['razao']; ?></h2>
-    <form action="insere_numeros.php" method="POST">
-        <label for="cabelereiro">Corte de cabelo:</label>
-        <input type="number" name="cabelereiro" id="cabelereiro" required min="0">
-        <label for="manicure">Manicure:</label>
-        <input type="number" name="manicure" id="manicure" required min="0">
-        <label for="acuidade">Acuidade Visual:</label>
-        <input type="number" name="acuidade" id="acuidade" required min="0">
-        <label for="atd_saude">Serviços de saúde:</label>
-        <input type="number" name="atd_saude" id="atd_saude" required min="0">
-        <label for="vacinas">Vacinas:</label>
-        <input type="number" name="vacinas" id="vacinas" required min="0">
-        <label for="telemedicina">Telemedicina:</label>
-        <input type="number" name="telemedicina" id="telemedicina" required min="0">
-        <label for="pulseiras">Pulseira de identificação:</label>
-        <input type="number" name="pulseiras" id="pulseiras" required min="0">
-        <input type="hidden" name="id_parada" id="id_parada" value="<?php echo $id_parada = $_GET['id_parada']; ?>">
-        <input type="hidden" name="acao" value="insere">
-        <input type="submit" value="Inserir">
-    </form>
+        <form action="insere_numeros.php" method="POST">
+            <label for="cabelereiro">Corte de cabelo:</label>
+            <input type="number" name="cabelereiro" id="cabelereiro" required min="0">
+            <label for="manicure">Manicure:</label>
+            <input type="number" name="manicure" id="manicure" required min="0">
+            <label for="acuidade">Acuidade Visual:</label>
+            <input type="number" name="acuidade" id="acuidade" required min="0">
+            <label for="atd_saude">Serviços de saúde:</label>
+            <input type="number" name="atd_saude" id="atd_saude" required min="0">
+            <label for="vacinas">Vacinas:</label>
+            <input type="number" name="vacinas" id="vacinas" required min="0">
+            <label for="telemedicina">Telemedicina:</label>
+            <input type="number" name="telemedicina" id="telemedicina" required min="0">
+            <label for="pulseiras">Pulseira de identificação:</label>
+            <input type="number" name="pulseiras" id="pulseiras" required min="0">
+            <input type="hidden" name="id_parada" id="id_parada" value="<?php echo $id_parada = $_GET['id_parada']; ?>">
+            <input type="hidden" name="acao" value="insere">
+            <input type="submit" value="Inserir">
+        </fieldset>
+        </form>
     </div>
-    </fieldset>
 </div>
 
 <?php
