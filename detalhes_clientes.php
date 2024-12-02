79388b26ffbc61f737a2673e1b0d0f5e5eca00b9
lafr <lafrodrigues@yahoo.com.br>
2024-12-02 16:35:41 -0300

    20241202-forms_final

--- detalhes_clientes.php
+++ detalhes_clientes.php
@@ -1,6 +1,36 @@
 <?php 
     include 'header.php';
 
+    if ($_POST) {
+        $id_cliente = $_POST['id_cliente'];
+        $nome = $_POST['nome'];
+        $razao = $_POST['razao'];
+        $sap = $_POST['sap'];
+        $cnpj = $_POST['cnpj'];
+        $endereco = $_POST['endereco'];
+        $bairro = $_POST['bairro'];
+        $cep = $_POST['cep'];
+        $cidade = $_POST['cidade'];
+        $uf = $_POST['uf'];
+        $representante = $_POST['representante'];
+        $ddd = $_POST['ddd'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $responsavel = $_POST['responsavel'];
+        $tipo = $_POST['tipo'];
+
+        $sql_update = "UPDATE clientes SET nome='$nome', razao='$razao', sap='$sap', cnpj='$cnpj', endereco='$endereco', bairro='$bairro', cep='$cep', cidade='$cidade', uf='$uf', representante='$representante', ddd='$ddd', telefone='$telefone', email='$email', responsavel='$responsavel' WHERE id_cliente='$id_cliente'";
+        $sql_update = str_replace("''", "NULL", $sql_update);
+        $result_update = mysqli_query($conn, $sql_update);
+
+        if ($result_update) {
+            echo "<script>alert('Cliente atualizado com sucesso!');</script>";
+            echo "<script>window.location.href='clientes.php';</script>";
+        } else {
+            echo "<script>alert('Erro ao atualizar cliente!');</script>";
+        }
+    }
+
     $sql_clientes = "SELECT * FROM clientes WHERE id_cliente = " . $_GET['id_cliente'];
     $result_clientes = mysqli_query($conn, $sql_clientes);
     $row_clientes = mysqli_fetch_assoc($result_clientes);
@@ -17,40 +47,15 @@
             $ver_tipo = 'Indefinido';
             break;
     }
-
-    if ($_POST) {
-        $razao = $_POST['razao'];
-        $sap = $_POST['sap'];
-        $cnpj = $_POST['cnpj'];
-        $endereco = $_POST['endereco'];
-        $bairro = $_POST['bairro'];
-        $cep = $_POST['cep'];
-        $cidade = $_POST['cidade'];
-        $uf = $_POST['uf'];
-        $representante = $_POST['representante'];
-        $ddd = $_POST['ddd'];
-        $telefone = $_POST['telefone'];
-        $email = $_POST['email'];
-        $responsavel = $_POST['responsavel'];
-        $tipo = $_POST['tipo'];
-
-        $sql_update = "UPDATE clientes SET razao = '$razao', sap = '$sap', cnpj = '$cnpj', endereco = '$endereco', bairro = '$bairro', cep = '$cep', cidade = '$cidade', uf = '$uf', representante = '$representante', ddd = '$ddd', telefone = '$telefone', email = '$email', responsavel = '$responsavel', tipo = '$tipo' WHERE id_cliente = " . $_GET['id_cliente'];
-        $result_update = mysqli_query($conn, $sql_update);
-
-        if ($result_update) {
-            echo "<script>alert('Cliente atualizado com sucesso!');</script>";
-            echo "<script>window.location.href='clientes.php';</script>";
-        } else {
-            echo "<script>alert('Erro ao atualizar cliente!');</script>";
-        }
-    }
 ?>
 
 <div class='bloco_centro'> 
     <div class='content'>
         <fieldset>
             <h2>Detalhes de <?php echo $row_clientes['razao']; ?></h2>
-            <form action='detalhes_clientes.php' method='post'>
+            <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+                <input type='hidden' name='id_cliente' value='<?php echo $_GET['id_cliente']; ?>'>
+                <label for="nome">Nome Fantasia</label><input type='text' name='nome' value='<?php echo $row_clientes['nome']; ?>'>
                 <label for="razao">Razão Social</label><input type='text' name='razao' value='<?php echo $row_clientes['razao']; ?>'>
                 <label for="sap">SAP</label><input type='text' name='sap' value='<?php echo $row_clientes['sap']; ?>'>
                 <label for="cnpj">CNPJ (apenas números)</label><input type='number' name='cnpj' value='<?php echo $row_clientes['cnpj']; ?>' >
@@ -64,11 +69,6 @@
                 <label for="telefone">Telefone</label><input type='tel' name='telefone' value='<?php echo $row_clientes['telefone']; ?>'>
                 <label for="email">Email</label><input type='text' name='email' value='<?php echo $row_clientes['email']; ?>'>
                 <label for="responsavel">Responsável</label><input type='text' name='responsavel' value='<?php echo $row_clientes['responsavel']; ?>'>
-                <label for="tipo">Cliente</label><input type='input' list='tipo' name='tipo' value='<?php echo $ver_tipo ?>'>
-                <datalist id='tipo'>
-                    <option value='1'>Vibra</option>
-                    <option value='2'>Cooper/Goodyear</option>
-                </datalist>
                 <input type='submit' value='Salvar'>
             </form>
         </fieldset> 
--- insere_cliente.php
+++ insere_cliente.php
@@ -6,4 +6,62 @@
      */
     
     include "header.php";
+
+    if ($_POST) {
+        $nome = $_POST['nome'];
+        $razao = $_POST['razao'];
+        $sap = $_POST['sap'];
+        $cnpj = $_POST['cnpj'];
+        $endereco = $_POST['endereco'];
+        $bairro = $_POST['bairro'];
+        $cep = $_POST['cep'];
+        $cidade = $_POST['cidade'];
+        $uf = $_POST['uf'];
+        $representante = $_POST['representante'];
+        $ddd = $_POST['ddd'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $responsavel = $_POST['responsavel'];
+        $tipo = $_POST['tipo'];
+
+        $sql_insert = "INSERT INTO clientes (nome, razao, sap, cnpj, endereco, bairro, cep, cidade, uf, representante, ddd, telefone, email, responsavel, tipo) VALUES ('$nome', '$razao', '$sap', '$cnpj', '$endereco', '$bairro', '$cep', '$cidade', '$uf', '$representante', '$ddd', '$telefone', '$email', '$responsavel', '$tipo')";
+        $sql_insert = str_replace("''", "NULL", $sql_insert);
+        $result_insert = mysqli_query($conn, $sql_insert);
+
+        if ($result_insert) {
+            echo "<script>alert('Cliente atualizado com sucesso!');</script>";
+            echo "<script>window.location.href='clientes.php';</script>";
+        } else {
+            echo "<script>alert('Erro ao atualizar cliente!');</script>";
+        }
+    }
 ?>
+
+<div class='bloco_centro'>
+    <div class='content'>
+        <fieldset>
+        <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+            <label for="nome">Nome Fantasia</label><input type='text' name='nome'>
+            <label for="razao">Razão Social</label><input type='text' name='razao'>
+            <label for="sap">SAP</label><input type='text' name='sap'>
+            <label for="cnpj">CNPJ</label><input type='text' name='cnpj'>
+            <label for="endereco">Endereço</label><input type='text' name='endereco'>
+            <label for="bairro">Bairro</label><input type='text' name='bairro'>
+            <label for="cep">CEP</label><input type='text' name='cep'>
+            <label for="cidade">Cidade</label><input type='text' name='cidade'>
+            <label for="uf">UF</label><input type='text' name='uf'>
+            <label for="representante">Representante</label><input type='text' name='representante'>
+            <label for="ddd">DDD</label><input type='text' name='ddd'>
+            <label for="telefone">Telefone</label><input type='text' name='telefone'>
+            <label for="email">E-mail</label><input type='text' name='email'>
+            <label for="responsavel">Responsável</label><input type='text' name='responsavel'>
+            <label for="tipo">Tipo</label>
+            <select name='tipo'>
+                <option value='1'>Vibra</option>
+                <option value='2'>Cooper</option>
+            </select>
+            <input type='submit' value='Inserir'>
+        </form>
+        </fieldset>
+    </div>
+</div>
